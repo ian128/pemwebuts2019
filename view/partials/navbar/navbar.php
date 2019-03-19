ea3267c3f2832ae9e1cc067915a8f669452c4176
Alessandro Ferdian <alessandroferdian128@gmail.com>
2019-03-20 00:07:55 +0700

    Blom kelar

--- view/partials/navbar/navbar.php
+++ view/partials/navbar/navbar.php
@@ -17,18 +17,4 @@
       </div>
    </div>
 </nav>
-<script>
-  $(document).ready(()=>{
-    $.post("../controller.php",
-    {'getFriends' : 1},
-    function (response){ 
-      let d=JSON.parse(response);
-      $('#listOfFriends').html(d.map(friends).join(''));
-    }) 
-    
-  })
-
-friends=({NamaLengkap,UserID})=>`
-    <a click=getValue() class="dropdown-item" id="friendEntries" value="${UserID}"><span>${NamaLengkap} </span><span>(@${UserID})</span></a>
-    `;
-</script>
+<script src="partials/navbar/navbar.js"></script>
